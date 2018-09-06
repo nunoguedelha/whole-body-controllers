769ea69de8f249d98e8137e3f9c7a67bcd4b7d7c
lrapetti <lor.rapetti@gmail.com>
2018-09-06 18:11:35 +0200

    Update

--- torque-controllers/impedance-control/app/robots/iCubGenova04/configRobot.m
+++ torque-controllers/impedance-control/app/robots/iCubGenova04/configRobot.m
@@ -16,26 +16,27 @@ WBTConfigRobot.LocalName = 'WBT';
 %                                      'l_hip_pitch','l_hip_roll','l_hip_yaw','l_knee','l_ankle_pitch','l_ankle_roll', ...
 %                                      'r_hip_pitch','r_hip_roll','r_hip_yaw','r_knee','r_ankle_pitch','r_ankle_roll'};
 WBTConfigRobot.ControlBoardsNames = {'right_leg'};
-WBTConfigRobot.ControlledJoints   = {'r_knee'};
+WBTConfigRobot.ControlledJoints   = {'r_ankle_pitch'};
 % WBTConfigRobot.ControlledJoints   = {'r_hip_pitch','r_hip_roll','r_hip_yaw','r_knee','r_ankle_pitch','r_ankle_roll'};
 
 % Gains of the torque-to-current control
 Kc = 0.0;
-Kv = 0.000655;
-Kt = 0.187259;
+Kv = 0.001343;
+Kt = -0.150921;
 % Kc = [ 0.0 0.0 0.0 0.0 0.0 0.0 ];
 % Kv = [ 0.001013 0.000458 0.000980 0.000655 0.001343 0.000520 ];
 % Kt = [ 0.142674 -0.158045 0.132443 0.187259 -0.150921 -0.096339 ];
+Kfc = 0.1;
 
 % References for the demo movements
 if MOVING
     
     % Impedance gains
-    Kp     = 44*diag(ones(1,ROBOT_DOF));
-    Kd     = 3.0*sqrt(Kp);
+    Kp     = 20*diag(ones(1,ROBOT_DOF));
+    Kd     = 2.5*sqrt(Kp);
     
     AMPLS  = 6.0*ones(1,ROBOT_DOF);
-    FREQS  = 0.5*ones(1,ROBOT_DOF);
+    FREQS  = 0.2*ones(1,ROBOT_DOF);
    
 else
     
