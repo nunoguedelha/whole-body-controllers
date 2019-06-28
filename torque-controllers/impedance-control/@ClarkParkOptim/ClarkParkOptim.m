c6e0e06c1c3577707b2990a06a65a4cb2bfdcafb
Nuno Guedelha <nuno.guedelha@iit.it>
2019-06-28 16:00:15 +0200

    Added optimization function to ClarkParkOptim class and improved analysis/plot script

--- torque-controllers/impedance-control/@ClarkParkOptim/ClarkParkOptim.m
+++ torque-controllers/impedance-control/@ClarkParkOptim/ClarkParkOptim.m
@@ -21,6 +21,31 @@ classdef ClarkParkOptim < handle
             Iq = obj.CPT.phase2dirquad(dPhi(1),dPhi(2),obj.i1i2i3);
             errorVec = Iq-mean(Iq);
         end
+        
+        function [dPhiOpt, motShift, resnorm, exitflag, output] = optimizeReadingAxes(obj, dPhi0)
+            % Use an optimization approach to find the phase shifts that result in a constant Iq
+            [optimFunction,options] = ClarkParkOptim.getOptimConfig();
+            
+            % optimize
+            %
+            % Important note:
+            % - dPhi0 is the init vector for the optimization
+            % - dPhi is the main optimization variable (format set by dPhi0)
+            %
+            funcProps = functions(optimFunction);
+            funcName = funcProps.function;
+            switch funcName
+                case 'lsqnonlin'
+                    [dPhiOpt, resnorm, ~, exitflag, output, ~] ...
+                        = optimFunction(@(dPhi) obj.costFunction1(dPhi), ...
+                        dPhi0, [], [], options);
+                otherwise
+                    % We are not computing optimalDq, but just using a previous
+                    % result for a performance evaluation
+                    error('Solver not supported !');
+            end
+            motShift = obj.CPT.elec2rotorPhase(dPhiOpt,6)*180/pi;
+        end
     end
     
     methods (Static=true)
--- torque-controllers/impedance-control/plotMotorCurrentToPos.m
+++ torque-controllers/impedance-control/plotMotorCurrentToPos.m
@@ -1,7 +1,7 @@
 clear variables
 
 % joints = {'l_hip_pitch','l_hip_roll','l_hip_yaw','l_knee','l_ankle_pitch','l_ankle_roll'};
-joints = {'r_knee'};
+joints = {'r_hip_pitch'};
 
 % For each figure
 for figIdx=1
@@ -77,63 +77,118 @@ for figIdx=1
         'motor position (deg)','phase current (mA)',...
         'Ic meas','Ic filtered');
     
-    %% Compute expected Iq and adjust the "reading axes" to obtain a constant Iq
-    
-    % Remove the current offsets
-    meanIa = mean([qIFilt(vIdx).expIa])
-    meanIc = mean([qIFilt(vIdx).expIc])
-    qIa = qIa-meanIa;
-    qIc = qIc-meanIc;
-    qIb = -qIa-qIc;
+    %% Compute expected Iq, compensate the offsets and adjust the "reading axes" to obtain a constant Iq
     
     % Pre-compute all the Clark Park Trans. for the defined rotor positions
     CPT = ClarkParkTransform(pos);
     
-    dPhi0 = [1 1]'; % Elec Phase Shift
-    optimCtx = ClarkParkOptim(CPT,qIa,qIb,qIc);
+    % Prepare figure handler
+    figuresHandler = DiagPlotFiguresHandler('figs');
     
-    % Use an optimization approach to find the phase shifts that result in a constant Iq
-    [optimFunction,options] = ClarkParkOptim.getOptimConfig();
+    % Compute expected Iq without any compensation
+    expIq = CPT.phase2dirquad(0,0,[qIa;qIb;qIc]);
     
-    % optimize
-    %
-    % Important note:
-    % - dPhi0 is the init vector for the optimization
-    % - dPhi is the main optimization variable (format set by dPhi0)
-    %
-    funcProps = functions(optimFunction);
-    funcName = funcProps.function;
-    switch funcName
-        case 'lsqnonlin'
-            [dPhiOpt, resnorm, ~, exitflag, output, ~] ...
-                = optimFunction(@(dPhi) optimCtx.costFunction1(dPhi), ...
-                dPhi0, [], [], options);
-        otherwise
-            % We are not computing optimalDq, but just using a previous
-            % result for a performance evaluation
-            error('Solver not supported !');
-    end
+    % Remove the current offsets and plot
+    meanIa = mean([qIFilt(vIdx).expIa])
+    meanIc = mean([qIFilt(vIdx).expIc])
+    qIa_offComp = qIa-meanIa;
+    qIc_offComp = qIc-meanIc;
+    qIb_offComp = -qIa_offComp-qIc_offComp;
     
-    dPhiOpt
-    resnorm
-    exitflag
-    output
+    expIqOffComp = CPT.phase2dirquad(0,0,[qIa_offComp;qIb_offComp;qIc_offComp]);
     
-    motShift = CPT.elec2rotorPhase(dPhiOpt,6)*180/pi;
-    display(motShift);
-    
-    % Re-compute Ic with the optimized values
-    expIq = CPT.phase2dirquad(dPhiOpt(1),dPhiOpt(2),optimCtx.i1i2i3);
-    %expIq = CPT.phase2dirquad(0,0,optimCtx.i1i2i3);
-
-    % Plot expected Iq
+    % Plot expected Iq, compensated and not compensated
     Plotter.plot2dDataNfittedModel(...
-        [],[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig' num2str(figIdx)],...
+        figuresHandler,[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig-OffsetComp' num2str(figIdx)],...
         pos,expIq,[],[],...
         'motor position (deg)','quadrature current (mA)',...
         'Iq exp',[]);
     
+    Plotter.plot2dDataNfittedModel(...
+        figuresHandler,[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig-OffsetComp' num2str(figIdx)],...
+        pos,expIqOffComp,[],[],...
+        [],[],...
+        'Iq exp offset compensation.',[]);
+
+    % Compensate the phase shift errors only. Use an optimization approach to find the phase shifts that result in a
+    % constant Iq.
+    dPhi0 = [0 0]'; % Elec Phase Shift
+    optimCtx = ClarkParkOptim(CPT,qIa,qIb,qIc);
+    
+    [dPhiOpt, motShift, resnorm, exitflag, output] = optimCtx.optimizeReadingAxes(dPhi0)
+    
+    expIqPhaseShiftComp = CPT.phase2dirquad(dPhiOpt(1),dPhiOpt(2),optimCtx.i1i2i3);
+    
+    % Plot expected Iq, compensated and not compensated
+    Plotter.plot2dDataNfittedModel(...
+        figuresHandler,[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig-PhaseComp' num2str(figIdx)],...
+        pos,expIq,[],[],...
+        'motor position (deg)','quadrature current (mA)',...
+        'Iq exp',[]);
+    
+    Plotter.plot2dDataNfittedModel(...
+        figuresHandler,[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig-PhaseComp' num2str(figIdx)],...
+        pos,expIqPhaseShiftComp,[],[],...
+        'motor position (deg)','quadrature current (mA)',...
+        'Iq exp phase shift compensation',[]);
+    
+    % Compensate both offsets and phase shift errors.
+    clear optimCtx;
+    optimCtx = ClarkParkOptim(CPT,qIa_offComp,qIb_offComp,qIc_offComp);
+
+    [dPhiOpt, motShift, resnorm, exitflag, output] = optimCtx.optimizeReadingAxes(dPhi0)
+    
+    expIqPhaseOffShiftComp = CPT.phase2dirquad(dPhiOpt(1),dPhiOpt(2),optimCtx.i1i2i3);
+    
+    % Plot expected Iq, compensated and not compensated
+    Plotter.plot2dDataNfittedModel(...
+        figuresHandler,[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig-OffsetPhaseComp' num2str(figIdx)],...
+        pos,expIq,[],[],...
+        'motor position (deg)','quadrature current (mA)',...
+        'Iq exp',[]);
+    
+    Plotter.plot2dDataNfittedModel(...
+        figuresHandler,[System.toLatexInterpreterCompliant(joints{figIdx}) ' : expected quadrature Current = f(Position)'],['motorExpIq2pos-Fig-OffsetPhaseComp' num2str(figIdx)],...
+        pos,expIqPhaseOffShiftComp,[],[],...
+        'motor position (deg)','quadrature current (mA)',...
+        'Iq exp offset and phase shift compensation',[]);
     
     clear t; clear y;
 end
 
+%% Converting the cos, sin tables in the 2FOC code
+
+% Original tables
+cos_table = [32767,32763,32748,32723,32688,32643,32588,32523,32449,32364,32270,32165,32051,31928,31794,31651,31498,31336,31164,30982,30791,30591,30381,30163,29935,29697,29451,29196,28932,28659,28377];
+sin_table = [    0,  330,  660,  990, 1319, 1648, 1977, 2305, 2632, 2959, 3285, 3609, 3933, 4255, 4576, 4896, 5214, 5531, 5846, 6159, 6470, 6779, 7087, 7392, 7694, 7995, 8293, 8588, 8881, 9171, 9459];
+
+% computed tables
+cos_tbl = round(cos((0:1:30)/180*pi)*32767);
+sin_tbl = round(sin((0:1:30)/180*pi)*32767/sqrt(3));
+
+% Identified phase shift errors
+dPhi = [0.0324 0.3422];
+
+% Since the Clark-Park transform is:
+% clarkT = @(dPhi1,dPhi2) sqrt(2/3)*[
+%     1 cos(-2*pi/3+dPhi1) cos(-4*pi/3+dPhi2)
+%     0 sin(-2*pi/3+dPhi1) sin(-4*pi/3+dPhi2)
+%     1/sqrt(2) 1/sqrt(2) 1/sqrt(2)
+%     ];
+% 
+% We have to substract the identified dPhi from the cos/sin arguments in the tables
+
+% converted tables
+cos_table_dphi1 = round(cos((0:1:30)/180*pi-dPhi(1))*32767);
+sin_table_dphi1 = round(sin((0:1:30)/180*pi-dPhi(1))*32767/sqrt(3));
+
+cos_table_dphi2 = round(cos((0:1:30)/180*pi-dPhi(2))*32767);
+sin_table_dphi2 = round(sin((0:1:30)/180*pi-dPhi(2))*32767/sqrt(3));
+
+clarkT = @(dPhiAxis2,dPhiAxis3) sqrt(2/3)*[
+    1 cos(-2*pi/3+dPhiAxis2) cos(-4*pi/3+dPhiAxis3)
+    0 sin(-2*pi/3+dPhiAxis2) sin(-4*pi/3+dPhiAxis3)
+    1/sqrt(2) 1/sqrt(2) 1/sqrt(2)
+    ];
+
+clarkTcorr = clarkT(dPhi(1),dPhi(2))./clarkT(0,0);
